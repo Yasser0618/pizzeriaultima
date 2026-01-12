699cbb9df2e335967bb2fe442501bade29216207
Yasser0618 <yas.iraola@gmail.com>
2026-01-12 10:20:00 +0100

    pizzeria ultima commit

---
+++ ae04/database/migrations/2026_01_08_121437_create_pizza_ingrediente_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pizza_ingrediente', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pizza_id')
+            ->contrained()
+            ->onDelete('cascade');
+
+            $table->foreignId('ingrediente_id')
+            ->contrained()
+            ->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pizza_ingrediente');
+    }
+};
