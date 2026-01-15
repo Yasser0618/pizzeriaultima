75f8f56c8bc2b1f9e763d68e4a019ec43aeb4866
Yasser0618 <yas.iraola@gmail.com>
2026-01-15 12:55:54 +0100

    Implementaciion de layouts y un menu general

---
+++ ae04/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('titulo', 'Pizzeria Ultima')</title>
+</head>
+<body>
+    {{-- Menú de navegación --}}
+    @include('partials.menu')
+
+    {{-- Contenido de cada vista --}}
+    <main>
+        @yield('contenido')
+    </main>
+</body>
+</html>
---
+++ ae04/resources/views/partials/menu.blade.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('pizzas.showAllPizzas') }}">Inicio</a>
+        </li>
+        <li>
+            <a href="{{ route('pizzas.create') }}">Crear Pizza</a>
+        </li>
+        <li>
+            <a href="{{ route('pizzas.showAllPizzas') }}">Crear Ingrediente</a>
+        </li>
+        <li>
+            <a href="{{ route('pizzas.showAllPizzas') }}">Listar ingredientes</a>
+        </li>
+    </ul>
+</nav>
